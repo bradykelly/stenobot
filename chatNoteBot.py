24f37f2dcc2f9c620a847d026eece5c82874cc02
Brady Kelly <brady@bradykelly.net>
2020-09-12 06:44:30 +0200

    Backend db insert working.

--- chatNoteBot.py
+++ chatNoteBot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import sys
+import backend
 from discord.ext.commands.cog import Cog
 from discord.ext.commands.core import bot_has_any_role
 from discord.ext.commands.errors import MissingRequiredArgument
@@ -12,7 +13,7 @@ BOT_NAME = "ChatNoteBot"
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 note_args = ["add", "find", "list", "del"]
 
-bot = commands.Bot(command_prefix="##", case_insensitive=True, name=BOT_NAME)
+bot = commands.Bot(command_prefix="##", case_insensitive=True, name=BOT_NAME, description="A note taking bot")
 
 class ChatNoteCommands(Cog, name="ChatNote"):
 
@@ -29,36 +30,43 @@ class ChatNoteCommands(Cog, name="ChatNote"):
             guild_count += 1
         print(f"{self.bot.user} has connected to Discord! In " + str(guild_count) + " guilds.")  
 
-        # Greet on each available channel.
-        text_channel_list = []
-        for guild in self.bot.guilds:
-            for channel in guild.channels:
-                if (channel.type == "Text"):
-                    text_channel_list.append(channel)
+        # # Greet on each available channel.
+        # text_channel_list = []
+        # for guild in self.bot.guilds:
+        #     for channel in guild.channels:
+        #         if (channel.type.name == "text"):
+        #             text_channel_list.append(channel)
 
-        for channel in text_channel_list:
-            await channel.send(f"{self.bot.user} is ready and waiting.")     
+        # for channel in text_channel_list:            
+        #     await channel.send(f"{self.bot.user} is ready and waiting.")     
 
+    # 'note' command group
     @commands.group(
         help="Commands to help you manage your ChatNote notebook",
-        brief="Manage your notebook"
+        brief="Manage your notebook",
+        usage=f"[add | find | list | del] text. Default is add."
     )
     async def note(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.channel.send("First layer")
 
+    # Add command
     @note.command(
-        help="Add some text to your current notebook",
-        brief="<text to add>"
+        help="Adds some text to your current notebook, or the named notebook",
+        brief="Adds text to a notebook",
+        usage="<text to add>"
     )
     async def add(self, ctx, text):
         note_text = text.strip() 
-        await ctx.channel.send(r"add \"" + note_text + "\"")
+        # For now we always use the default notebook name, used when None is passed.
+        backend.insert_note(ctx.message.author.id, text, None) 
+        await ctx.channel.send(r"note added: " + note_text)
 
     @add.error
     async def add_handler(self, ctx, error):
+        # TODO Find out why send_help didn't work.
         if isinstance(error, MissingRequiredArgument) and error.param.name == "text":
-            await ctx.send_help("add")
+            await ctx.channel.send(f"Usage: {bot.command_prefix}note add <text>")
 
 
 bot.add_cog(ChatNoteCommands(bot))
